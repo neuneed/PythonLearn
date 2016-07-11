9e4cdc6b02fc1d919838bb45a590c5c9d8533d61
Lee <liyalin1991@qq.com>
2016-07-11 18:26:09 +0800

    no message

--- class004/python_class4.py
+++ class004/python_class4.py
@@ -2,56 +2,99 @@
 # https://www.hackerearth.com/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
 from PIL import Image
 
-ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
 
-def scale_image(image, new_width=100):
-    """Resizes an image preserving the aspect ratio.
-    """
-    (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
-    new_height = int(aspect_ratio * new_width)
+# else if  elif!
+print('===============if else elif===============')
+if bool(3>4):
+    print(123)
+elif bool (4==3):
+    print(312)
+else:
+    print('elif' , 111, '\n')
 
-    new_image = image.resize((new_width, new_height))
-    return new_image
+# list
+print('===============list===============')
+a= []
+print(type(a))  # Class
+print(bool(a))  # bool 有没有值
+print(a)
 
-def convert_to_grayscale(image):
-    return image.convert('L')
+b= [1,'hello','2',1]
+print(b)
+print(b[1])  # 索引
+print(b[:2])  # 不包含结束位置
+print(b[-1])  # 负数从右开始
+print('b的长度',len(b),'\nb的类型',type(b))
 
-def map_pixels_to_ascii_chars(image, range_width=25):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
+x=['meme','haha',1]    # 合并2个list
+b.extend(x)
+print('拼接后的b',b, "extend的对象是list\n")
 
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
+# 出现次数
+la = [1 ,2 ,3 , 1, 1,3,'haha']
+print('1的出现次数',la.count(1))
+print('haha的位置',la.index('haha'))
 
-    pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
-            pixels_in_image]
+la.insert(3,'meme')
+print('在第三位 insert meme',la)
 
-    return "".join(pixels_to_chars)
+# la.remove('meme') # 删除
+# print('删除后',la)
 
-def convert_image_to_ascii(image, new_width=100):
-    image = scale_image(image)
-    image = convert_to_grayscale(image)
+if 'm1eme' in la:
+    la.remove('meme')
+else:
+    print('m1eme不在la里面')
 
-    pixels_to_chars = map_pixels_to_ascii_chars(image)
-    len_pixels_to_chars = len(pixels_to_chars)
+print('pop3 前的la',la)
+la.pop(3)   # pop是移除index的元素
+# la.remove(3) # remove是移除元素
+print('pop3 后的la',la)
 
-    image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            xrange(0, len_pixels_to_chars, new_width)]
+# rg = range(0, 100,2)
+# print(rg)
 
-    return "\n".join(image_ascii)
+nb = [1,4,5,6,7,9,0,12]
+nb.sort()
+nb.sort(reverse=True) # 实现倒叙
 
-def handle_image_conversion(image_filepath):
-    image = Image.open(image_filepath)
-    print ("Unable to open image file {test.jpg}.".format(image_filepath=image_filepath))
-    return
+print('sort 排序',nb)
 
-    image_ascii = convert_image_to_ascii(image)
-    print (image_ascii)
+# print(help(list))
 
-if __name__=='__main__':
-    import sys
+str = 'haha,nihao\mnade;shabi'
+print(str.split(';')) #分割字符串
 
-    image_file_path = sys.argv[1]
-    handle_image_conversion(image_file_path)
+name = ['Roland','Lee','haha']
+print('...'.join(name),'\n')
+
+
+# for
+print('===============for 语句===============')
+
+aligut = []
+for i in range(100):
+    if i % 3 == 0:
+        aligut.append(i)
+print('写法1',aligut)
+
+bligut = [i for i in range(1,100) if i%3==0]
+print('写法2',bligut)
+
+cligut = list(range(3,100,3))  # python3 range外面要加个list!!!
+print('写法3',cligut)
+
+# s = "  \t a string example\t  "
+# s = s.strip()
+# print('s is',s)  # 去除空格
+
+
+maybag = [' glass',  '   apple', 'green leaf  ']
+for one in maybag:
+    maybag.append(123)
+
+
+print('bag is' ,maybag)
+
+
+
